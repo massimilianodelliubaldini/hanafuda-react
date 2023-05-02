162b0a04c812194d5167f9ce93214035f6184c31
mpm11011 <mark.mauriello@uconn.edu>
2023-05-01 22:34:07 -0400

    Much progress. React bad.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,3 @@
-function shuffle(deck) {
-	for (let i = deck.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var t = deck[i];
-		deck[i] = deck[j];
-		deck[j] = t;
-	}
-
-	return deck;
-}
-
 function setupKoiKoiGame() {
 	return ReactDOM
 		.createRoot(document.getElementById("hanafuda"))
@@ -18,4 +7,4 @@ function setupKoiKoiGame() {
 		));
 }
 
-window.addEventListener("DOMContentLoaded", function (e) { });
+//window.addEventListener("DOMContentLoaded", function (e) { });
--- scripts/koikoigame.js
+++ scripts/koikoigame.js
@@ -1,4 +1,5 @@
 const allCards = [
+
 	{ id: "1-L",	key: "1-L",		month: "January",	flower: "Pine",				name: "Crane",			points: 20,	isLight: true , isSeed: false, isSlip: false, isPoetry: false, isBlue: false, isChaff: false, image: "https://upload.wikimedia.org/wikipedia/commons/1/15/Hanafuda_January_Hikari_Alt.svg" },
 	{ id: "1-P",	key: "1-P",		month: "January",	flower: "Pine",				name: "Poetry Slip",	points: 5,	isLight: false, isSeed: false, isSlip: true , isPoetry: true , isBlue: false, isChaff: false, image: "https://upload.wikimedia.org/wikipedia/commons/4/44/Hanafuda_January_Tanzaku_Alt.svg" },
 	{ id: "1-C1",	key: "1-C1",	month: "January",	flower: "Pine",				name: "Chaff",			points: 1,	isLight: false, isSeed: false, isSlip: false, isPoetry: false, isBlue: false, isChaff: true , image: "https://upload.wikimedia.org/wikipedia/commons/6/62/Hanafuda_January_Kasu_1_Alt.svg" },
@@ -60,64 +61,353 @@ const allCards = [
 	{ id: "12-C3",	key: "12-C3",	month: "December",	flower: "Paulownia",		name: "Chaff",			points: 1,	isLight: false, isSeed: false, isSlip: false, isPoetry: false, isBlue: false, isChaff: true , image: "https://upload.wikimedia.org/wikipedia/commons/8/81/Hanafuda_December_Kasu_Alt.svg" },
 ];
 
-const allAreas = [
-	{ id: "oppohand",	key: "oppohand",	name: "Opponent's Hand",		owner: "opponent",	canView: false, canPull: false, canPut: false, },
-	{ id: "oppocoll",	key: "oppocoll",	name: "Opponent's Collection",	owner: "opponent",	canView: true , canPull: false, canPut: false, },
-	{ id: "deck",		key: "deck",		name: "The Deck",				owner: "",			canView: true , canPull: true , canPut: false, },
-	{ id: "board",		key: "board",		name: "The Board",				owner: "",			canView: true , canPull: true , canPut: true , },
-	{ id: "yourcoll",	key: "yourcoll",	name: "Your Collection",		owner: "you",		canView: true , canPull: false, canPut: true , },
-	{ id: "yourhand",	key: "yourhand",	name: "Your Hand",				owner: "you",		canView: true , canPull: true , canPut: false, },
+const allSets = [
+
+	{ id: "5L",		key: "5L",		name: "Five Lights",					points: (collection) => { return 15;	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isLight, 5); }},
+	{ id: "4L",		key: "4L",		name: "Four Lights",					points: (collection) => { return 8; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isLight, 4) && !containsCard(collection, "11-L") }},
+	{ id: "4LR",	key: "4LR",		name: "Four Lights with Rain",			points: (collection) => { return 7; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isLight, 4) &&  containsCard(collection, "11-L") }},
+	{ id: "3L",		key: "3L",		name: "Three Lights",					points: (collection) => { return 6; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isLight, 3) && !containsCard(collection, "11-L") }},
+	{ id: "MV",		key: "MV",		name: "Moon Viewing",					points: (collection) => { return 5; 	},													requirement: (collection) => { return containsCard(collection, "9-S") && containsCard(collection, "8-L") }},
+	{ id: "CBV",	key: "CBV",		name: "Cherry Blossom Viewing",			points: (collection) => { return 5; 	},													requirement: (collection) => { return containsCard(collection, "9-S") && containsCard(collection, "3-L") }},
+	{ id: "BDB",	key: "BDB",		name: "Boar-Deer-Butterflies",			points: (collection) => { return 5; 	},													requirement: (collection) => { return containsCard(collection, "7-S") && containsCard(collection, "10-S") && containsCard(collection, "6-S") }},
+	{ id: "AAC",	key: "AAC",		name: "Red and Blue Poetry Slips",		points: (collection) => { return 10; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isPoetry,	3) && containsAtLeast(collection, (card) => card.isBlue, 5); }},
+	{ id: "RP",		key: "RP",		name: "Red Poetry Slips",				points: (collection) => { return 5; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isPoetry,	5);		}},
+	{ id: "BP",		key: "BP",		name: "Blue Poetry Slips",				points: (collection) => { return 5; 	},													requirement: (collection) => { return containsAtLeast(collection, (card) => card.isBlue,	5);		}},
+	{ id: "M",		key: "M",		name: "Month",							points: (collection) => { return 4;		},													requirement: (collection) => { return containsFourOfAKind(collection)								}},
+	{ id: "P",		key: "5P",		name: "Slips",							points: (collection) => { return countCards(collection, (card) => card.isSlip)	- 4;	},	requirement: (collection) => { return containsAtLeast(collection, (card) => card.isSlip,	5);		}},
+	{ id: "S",		key: "5S",		name: "Seeds",							points: (collection) => { return countCards(collection, (card) => card.isSeed)	- 4;	},	requirement: (collection) => { return containsAtLeast(collection, (card) => card.isSeed,	5);		}},
+	{ id: "C",		key: "C",		name: "Chaff",							points: (collection) => { return countCards(collection, (card) => card.isChaff)	- 9;	},	requirement: (collection) => { return containsAtLeast(collection, (card) => card.isChaff,	10);	}},
 ];
+
+const allMonths = [
+	"January",	
+	"February",	
+	"March",		
+	"April",		
+	"May",		
+	"June",		
+	"July",		
+	"August",	
+	"September",
+	"October",	
+	"November",
+	"December",
+];
+
+var countCards = function(collection, predicate) {
+	return collection.filter(predicate).length;
+};							
+
+var containsAtLeast = function(collection, predicate, numInstances) {
+	return countCards(collection, predicate) >= numInstances;
+};
+
+var containsCard = function(collection, cardId) {
+	return containsAtLeast(collection, (card) => card.id == cardId, 1);
+};
+
+var containsFourOfAKind = function(collection) {
+	for (var month of allMonths) { 
+		if (containsAtLeast(collection, (card) => card.month = month, 4)) {
+			return true;
+		}
+	};
+
+	return false;
+};
+
+var containsFourPairs = function(collection) {
+	var pairs = 0;
+	for (var month of allMonths) { 
+		if (containsAtLeast(collection, (card) => card.month = month, 2)) {
+			pairs += 1;
+		}
+	};
+
+	return pairs == 4;
+};
+
+var shuffleDeck = function(deck) {
+	for (let i = deck.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var t = deck[i];
+		deck[i] = deck[j];
+		deck[j] = t;
+	}
+
+	return deck;
+};
 
 function KoiKoiGame() {
 
-	const [gameAreas, setGameAreas] = React.useState(allAreas);
+	const [opponentHand, setOpponentHand]				= React.useState([]);
+	const [opponentCollection, setOpponentCollection]	= React.useState([]);
+	const [opponentScore, setOpponentScore]				= React.useState([]);
+	const [deck, setDeck]								= React.useState(shuffleDeck([...allCards]));
+	const [board, setBoard]								= React.useState([]);
+	const [yourCollection, setYourCollection]			= React.useState([]);
+	const [yourHand, setYourHand]						= React.useState([]);
+	const [yourScore, setYourScore]						= React.useState([]);
+	const [dealer, setDealer]							= React.useState("");
+	const [round, setRound]								= React.useState(0);
+	const [selectedCard, setSelectedCard]				= React.useState({});
+	const [availableMatches, setAvailableMatches]		= React.useState([]);
+	const [isTopDeck, setIsTopDeck]						= React.useState(false);
+	const [koiKoi, setKoiKoi]							= React.useState("");
 
-	var addGameArea = function(gameArea) {
-		setGameAreas([...gameAreas, gameArea]);
+	const determineDealer = () => {
+		console.log("determineDealer");
 	};
 
-	var addCardToArea = function(card, gameArea) {
-		gameArea.addCard(card);
-		setGameAreas([gameAreas.filter(a => a.id !== gameArea.id)]);
-		setGameAreas([...gameAreas, gameArea]);
+	const startRound = () => {
+		console.log("startRound");
+
+		var clone = shuffleDeck([...allCards]);
+		setYourHand(clone.slice(0, 8));
+		setOpponentHand(clone.slice(8, 16));
+		setBoard(clone.slice(16, 24));
+		setDeck(clone.slice(24, 48));
+
+		setYourCollection([]);
+		setOpponentCollection([]);
+		setSelectedCard({});
+		setAvailableMatches([]);
+
+		setRound(round + 1);
 	};
 
-	var removeCardFromArea = function(card, gameArea) {
-		gameArea.removeCard(card);
-		setGameAreas([gameAreas.filter(a => a.id !== gameArea.id)]);
-		setGameAreas([...gameAreas, gameArea]);
-	};
+	const checkForPreRoundSets = () => {
+		console.log("checkForPreRoundSets");
 
-	var moveCard = function(card, fromArea, toArea) {
-		addCardToArea(card, toArea);
-		removeCardFromArea(card, fromArea);
-	};
+		if (containsFourOfAKind(board) || containsFourPairs(board)) {
+			setYourScore([...yourScore, 0]);
+			setOpponentScore([...opponentScore, 0]);
 
-	var shuffleDeck = function() {
-		for (let i = allCards.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var t = allCards[i];
-			allCards[i] = allCards[j];
-			allCards[j] = t;
+			endRound();
 		}
+		else if (containsFourOfAKind(yourHand) || containsFourPairs(yourHand)) {
+			setYourScore([...yourScore, 6]);
+			setDealer("you");
 
-		for (let i = allCards.length - 1; i > 0; i--) {
-			addCardToArea(allCards[i], gameAreas.find(f => f.key == "deck"));
+			endRound();
+		}
+		else if (containsFourOfAKind(opponentHand) || containsFourPairs(opponentHand)) {
+			setOpponentScore([...opponentScore, 6]);
+			setDealer("opponent");
+
+			endRound();
 		}
 	};
 
-	return React.createElement(
-		"div", { className: "koikoigame", onLoad: shuffleDeck() }, 
-		gameAreas.map((a) => React.createElement(
-			GameArea, {
-				id: 		a.id,
-				key: 		a.key,
-				name: 		a.name,
-				owner: 		a.owner,
-				canView: 	a.canView,
-				canPull: 	a.canPull,
-				canPut: 	a.canPut,
+	const selectCardFromHand = (card) => {
+		console.log("selectCardFromHand");
+
+		setSelectedCard(card);
+	};
+
+	const evaluateMatches = (card) => {
+		console.log("evaluateMatches");
+
+		return board.filter(c => c.month == card.month);
+	};
+
+	const captureMatchingCards = (firstCard, secondCard) => {
+		console.log("captureMatchingCards");
+
+		setYourCollection([...yourCollection, firstCard, secondCard]);
+		setYourHand(yourHand.filter(c => c.id !== firstCard.id));		// Filter both your hand and the deck, in case you capture with the top deck. 
+		setDeck(deck.filter(c => c.id !== firstCard.id));
+		setBoard(board.filter(c => c.id !== secondCard.id));
+
+		setSelectedCard({});
+		setAvailableMatches([]);
+
+		setIsTopDeck(!isTopDeck);
+
+	};
+
+	const surrenderSelectedCard = (card) => {
+		console.log("surrenderSelectedCard");
+
+		setBoard([...board, card]);
+		setYourHand(yourHand.filter(c => c.id !== card.id));
+
+		setSelectedCard({});
+		setAvailableMatches([]);
+
+		setIsTopDeck(!isTopDeck);
+	};
+
+	const revealTopDeck = () => {
+		console.log("revealTopDeck");
+
+		var card = deck[0];
+		var matches = evaluateMatches(card);
+		setSelectedCard(card);
+		setAvailableMatches(matches);
+
+		if (matches.length == 0) {
+
+			setBoard([...board, card]);
+			setDeck(deck.filter(c => c.id !== card.id));
+
+			setSelectedCard({});
+			setAvailableMatches([]);
+
+			setIsTopDeck(false);
+			ReactDOM
+				.createRoot(document.getElementById("deck"))
+				.render(React.createElement("div", { style: { margin: 2, height: 150, width: 92, display: "inline-block", backgroundColor: "black"} }));
+		}
+		else {
+
+			ReactDOM
+				.createRoot(document.getElementById("deck"))
+				.render(React.createElement("div", { className: "hanafudacard", style: { height: 150, width: 92, margin: 2, display: "inline-block" }}, 
+						React.createElement("img", { src: card.image })));
+		}
+	};
+
+	const checkForCompletedSets = () => {
+		console.log("checkForCompletedSets");
+	};
+
+	const callKoiKoi = () => {
+		console.log("callKoiKoi");
+	};
+
+	const endRound = () => {
+		console.log("endRound");
+	};
+
+	const endGame = () => {
+		console.log("endGame");
+	};
+
+	const handleClick = (card) => {
+		console.log("handleClick");
+
+		// Is this the top card of the deck?
+		if (isTopDeck) {
+
+			// Is the card you clicked in your hand or on the board?
+			if (containsCard(yourHand, card.id)) {
+
+				console.log("You cannot select cards in your hand when the selected card is the top deck.");
 			}
-	)));
+			else {
+
+				// Is your clicked card an available match?
+				if (containsCard(availableMatches, card.id)) {
+
+					// Capture the matching cards.
+					captureMatchingCards(selectedCard, card);
+				}
+				else {
+
+					console.log("Your selected card and clicked card don't match.");
+				}
+			}
+		}
+		else {
+
+			// Is the card you clicked in your hand or on the board?
+			if (containsCard(yourHand, card.id)) {
+
+				// Have you clicked on the selected card?
+				if (selectedCard.id == card.id) {
+
+					// Are there available matches with this card?
+					if (availableMatches.length == 0) {
+
+						// Surrender the selected card.
+						surrenderSelectedCard(card);
+					}
+					else {
+
+						console.log("Either select a matching card from the board or select another card in hand.");
+					}
+				}
+				else {
+
+					// Selected a new card.
+					selectCardFromHand(card);
+					setAvailableMatches(evaluateMatches(card));
+				}	
+			}
+			else {
+
+				// Is there already a selected card?
+				if (selectedCard.id == null) {
+
+					console.log("You have not selected a card yet.");
+				}
+				else {
+
+					// Is your clicked card an available match?
+					if (containsCard(availableMatches, card.id)) {
+
+						// Capture the matching cards.
+						captureMatchingCards(selectedCard, card);
+					}
+					else {
+
+						console.log("Your selected card and clicked card don't match.");
+					}
+				}
+			}
+		}
+	};
+
+	React.useEffect(() => {
+		if (isTopDeck) {
+			revealTopDeck();
+		}
+		else {
+			ReactDOM
+				.createRoot(document.getElementById("deck"))
+				.render(React.createElement("div", { style: { margin: 2, height: 150, width: 92, display: "inline-block", backgroundColor: "black"} }));
+		}
+	}, [isTopDeck]);
+
+	return React.createElement("div", { className: "koikoigame" },
+		React.createElement("div", null,
+			React.createElement("button", { onClick: startRound }, "Start a round")),
+		React.createElement("div",  null,
+			React.createElement("span", null, "Round " + round + " / 12")),
+		React.createElement("h2", null, "Opponent's Hand"),
+		React.createElement("div", { id: "opponenthand" }, 
+			opponentHand.map((card) => 
+				React.createElement("div", { style: { margin: 2, height: 150, width: 92, display: "inline-block", backgroundColor: "black"} }))),
+
+		React.createElement("h2", null, "Opponent's Collection"),
+		React.createElement("div", { id: "opponentcollection" }, 
+			opponentCollection.map((card) => 
+				React.createElement("div", { className: "hanafudacard", style: { height: 150, width: 92, margin: 2, display: "inline-block" }}, 
+					React.createElement("img", { src: card.image })))),
+
+		React.createElement("h2", null, "The Deck"),
+		React.createElement("div", { id: "deck" }, 
+			React.createElement("div", { style: { margin: 2, height: 150, width: 92, display: "inline-block", backgroundColor: "black"} })),
+
+		React.createElement("h2", null, "The Board"),
+		React.createElement("div", { id: "board" }, 
+			board.map((card) => 
+				React.createElement("div", { className: "hanafudacard", style: { height: 150, width: 92, margin: 2, display: "inline-block" }}, 
+					React.createElement("button", { onClick: () => handleClick(card), style: { height: "inherit", width: "inherit", padding: 0, border: "none", background: "none" }},
+						React.createElement("img", { src: card.image }))))),
+
+		React.createElement("h2", null, "Your Collection"),
+		React.createElement("div", { id: "yourcollection" }, 
+			yourCollection.map((card) => 
+				React.createElement("div", { className: "hanafudacard", style: { height: 150, width: 92, margin: 2, display: "inline-block" }}, 
+					React.createElement("img", { src: card.image })))),
+
+		React.createElement("h2", null, "Your Hand"),
+		React.createElement("div", { id: "yourhand" }, 
+			yourHand.map((card) => 
+				React.createElement("div", { className: "hanafudacard", style: { height: 150, width: 92, margin: 2, display: "inline-block" }},
+					React.createElement("button", { onClick: () => handleClick(card), style: { height: "inherit", width: "inherit", padding: 0, border: "none", background: "none" }},
+						React.createElement("img", { src: card.image }))))),
+	);
 }
